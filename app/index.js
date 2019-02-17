39f37c1adead9ce2789db8280474855cd5a5c89f
Kyle Doherty <l00127301@student.lyit.ie>
2019-02-17 21:35:34 +0000

    sms functionality added

--- app/index.js
+++ app/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain-classes/blockchain');
 const P2PServer = require('./p2pServer');
+const {TWILIO_SID, TWILIO_AUTH_TOKEN} = require('../config');
+var verifiedVoters = require('./verifiedVoters.json'); 
 
 //If there isnt a env variable passed into the npm run dev
 //the default port is 3001.
@@ -10,6 +12,11 @@ const HTTP_PORT = process.env.HTTP_PORT || 3001;
 //Decalre the express server
 const app = express();
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 //Create the instance of the blockchain
 const blockchain = new Blockchain;
@@ -18,6 +25,10 @@ const blockchain = new Blockchain;
 const p2pServer = new P2PServer(blockchain);
 
 //Current routes atm, will try to move them to a routes.js soon
+app.get('/voters', (req, res) => {
+    res.json(verifiedVoters.voters);
+});
+
 app.get('/blocks', (req, res) => {
     res.json(blockchain.chain);
 });
@@ -27,6 +38,20 @@ app.post('/mine', (req, res) => {
     console.log(`New block added: ${block.toString()}`);
     p2pServer.updateChains();
     res.redirect('/blocks');
+});
+
+app.post('/sendsms', (req, res) => {
+    var phone = req.body.data;
+
+    const client = require('twilio')(TWILIO_SID, TWILIO_AUTH_TOKEN);
+
+    client.messages
+    .create({
+        body: 'Your private key is: "xxxxx" ',
+        from: '+12533365589',
+        to: '+353831362447'
+    })
+    .then(message => console.log(message.sid));
 });
 
 //Start the express server and p2p server
